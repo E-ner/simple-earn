e61df96eee7704528e2f5838a0ab9abf807897db
e-ner <nsengiyumva603@gmail.com>
2026-03-21 00:11:14 +0200

    fix: add key token to the system

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -18,6 +18,7 @@ declare module "next-auth" {
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma) as any,
+  secret: process.env.NEXTAUTH_SECRET,
   session: { strategy: "jwt" },
   pages: { signIn: "/login" },
   providers: [
